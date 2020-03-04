08eba03d025f7c2e3255d3b4a99381cde0caba6a
Rohan Puri <rohan.puri@seagate.com>
2020-03-04 09:02:23 +0000

    EOS-3584 - Add missing efs_internal.h file

---
+++ efs/src/efs/efs_internal.h
@@ -0,0 +1,28 @@
+/*
+ * Filename:         efs_internal.h
+ * Description:      EOS file system internal APIs
+
+ * Do NOT modify or remove this copyright and confidentiality notice!
+ * Copyright (c) 2019, Seagate Technology, LLC.
+ * The code contained herein is CONFIDENTIAL to Seagate Technology, LLC.
+ * Portions are also trade secret. Any use, duplication, derivation,
+ * distribution or disclosure of this code, for any reason, not expressly
+ * authorized is prohibited. All other rights are expressly reserved by
+ * Seagate Technology, LLC.
+
+ This file contains EFS file system internal APIs
+*/
+
+#ifndef _EFS_INTERNAL_H
+#define _EFS_INTERNAL_H
+
+#include <efs.h>
+#include <dstore.h>
+
+/** Get the extstore object identifier for the passed efs inode */
+int efs_ino_to_oid(efs_ctx_t *ctx, const efs_ino_t *ino, dstore_oid_t *oid);
+
+/** Delete the ino-kfid key-val pair from the kvs. Called during unlink/rm. */
+int efs_del_oid(efs_ctx_t *ctx, const efs_ino_t *ino);
+
+#endif
