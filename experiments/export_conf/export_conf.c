4ba1643ea5331ff7c4aab8f8c2545c2846fc59dc
Jatinder Kumar <jatinder.kumar@seagate.com>
2020-04-14 01:22:52 -0600

    EOS-ganesha_config_gen: added block create

--- experiments/export_conf/export_conf.c
+++ experiments/export_conf/export_conf.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/sendfile.h>
 #include <string.h>
 
 struct filesystem_id {
@@ -18,11 +21,10 @@ struct export_option {
 	struct filesystem_id fs_id;
 };
 
-int efs_ganesha_conf(FILE * file, struct export_option *options)
+int create_block(FILE * file, struct export_option *options)
 {
-
 	fprintf(file, "\nexport {\n");
-	fprintf(file, "\tExport_id = %d;\n", options->export_id );
+	fprintf(file, "\tExport_id = %d;\n", options->export_id);
 	fprintf(file, "\tPath = %s;\n", options->path);
 	fprintf(file, "\tPseudo = /%s;\n", options->path);
 	fprintf(file, "\tFSAL {\n");
@@ -39,11 +41,31 @@ int efs_ganesha_conf(FILE * file, struct export_option *options)
 	fprintf(file, "\t\tprotocols = %d;\n", options->protocols);
 	fprintf(file, "\t}\n");
 	fprintf(file, "}\n");
-
 	// confirm test.conf with ganesha helper api config_parse and retunr error code
-
 	return 0;
+}
 
+int OSCopyFile(const char* source, const char* destination)
+{
+	int input, output;
+	if ((input = open(source, O_RDONLY)) == -1)
+	{
+		return -1;
+	}
+	if ((output = creat(destination, 0660)) == -1)
+	{
+		close(input);
+		return -1;
+	}
+
+	off_t bytesCopied = 0;
+	struct stat fileinfo = {0};
+	fstat(input, &fileinfo);
+	int result = sendfile(output, input, &bytesCopied, fileinfo.st_size);
+
+	close(input);
+	close(output);
+	return result;
 }
 
 int main()
@@ -51,12 +73,6 @@ int main()
 	int rc = 0;
 	struct export_option options;
 	char *file_path = "./test.conf";
-
-	FILE *file = fopen(file_path, "a+");
-	if (file == NULL) {
-		fprintf(stderr, "Can't create file for writing\n");
-		return -1;
-	}
 
 	options.export_id = 11;
 	memcpy(options.path, "test", sizeof("test"));
@@ -68,6 +84,13 @@ int main()
 	options.fs_id.major = 192;
 	options.fs_id.minor = 168;
 
-	rc = efs_ganesha_conf(file, &options);
+	rc = OSCopyFile(file_path, "/etc/ganesha/tmpganesha");
+
+	FILE *file = fopen("/etc/ganesha/tmpganesha", "a+");
+	if (file == NULL) {
+		fprintf(stderr, "Can't create file for writing\n");
+		return -1;
+	}
+	rc = create_block(file, &options);
 	return 0;
 }
