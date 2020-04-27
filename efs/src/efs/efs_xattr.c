744ea79a238b8007f9326d49688c2fd832d63f27
Subhash Arya <subhash.arya@seagate.com>
2020-04-27 02:51:41 +0000

    EOS-6916: Use node_id for xattr

--- efs/src/efs/efs_xattr.c
+++ efs/src/efs/efs_xattr.c
@@ -16,20 +16,31 @@
 #include <common/log.h> /* log_*() */
 #include <common/helpers.h> /* RC_LABEL*() */
 #include <efs.h> /* efs_* */
-#include <dstore.h> /* dstore_oid_t */
 #include <md_xattr.h> /* md_xattr_exists */
 #include "efs_internal.h" /*efs_ino_to_oid */
 #include <errno.h> /* ERANGE */
 #include <string.h> /* memcpy */
 #include <sys/xattr.h> /* XATTR_CREATE */
-#include "kvtree.h"
+
+/** @TODO: Move this function to a common header file whenever other efs apis use it. */
+static inline int efs_ino_to_nodeid(const efs_ino_t *ino, node_id_t *nid)
+{
+	int rc = 0;
+	dassert(ino != NULL);
+	dassert(nid != NULL);
+
+	nid->f_hi = *ino;
+	nid->f_lo = 0;
+
+	return rc;
+}
 
 int efs_setxattr(struct efs_fs *efs_fs, const efs_cred_t *cred,
 		 const efs_ino_t *ino, const char *name, char *value,
 		 size_t size, int flags)
 {
 	int rc;
-	dstore_oid_t oid;
+	node_id_t nid;
 	bool xattr_exists;
 
 	dassert(cred && ino && name && value);
@@ -43,12 +54,11 @@ int efs_setxattr(struct efs_fs *efs_fs, const efs_cred_t *cred,
 	RC_WRAP_LABEL(rc, out, efs_access, efs_fs, cred, ino,
 		      EFS_ACCESS_WRITE);
 
-	RC_WRAP_LABEL(rc, out, efs_ino_to_oid, efs_fs, ino, &oid);
+	RC_WRAP_LABEL(rc, out, efs_ino_to_nodeid, ino, &nid);
 
 	if ((flags == XATTR_CREATE) || (flags == XATTR_REPLACE)) {
-		RC_WRAP_LABEL(rc, out, md_xattr_exists, &(efs_fs->kvtree->index)
-		,
-			     (obj_id_t *)&oid, name, &xattr_exists);
+		RC_WRAP_LABEL(rc, out, md_xattr_exists, &(efs_fs->kvtree->index),
+			      &nid, name, &xattr_exists);
 
 		if (flags ==  XATTR_CREATE && xattr_exists) {
 			rc  = -EEXIST;
@@ -64,12 +74,13 @@ int efs_setxattr(struct efs_fs *efs_fs, const efs_cred_t *cred,
 		}
 	}
 
-	RC_WRAP_LABEL(rc, out, md_xattr_set, &(efs_fs->kvtree->index), (obj_id_t *)&oid, name,
-		      value, size);
+	RC_WRAP_LABEL(rc, out, md_xattr_set, &(efs_fs->kvtree->index),
+		      &nid, name, value, size);
 
 out:
-	log_trace("ctx=%p *ino=%llu oid=%" PRIx64 ":%" PRIx64 " rc=%d name=%s val=%p size=%lu",
-		   efs_fs, *ino, oid.f_hi, oid.f_lo, rc, name, value, size);
+	log_trace("ctx=%p *ino=%llu nid=%" PRIx64 ":%" PRIx64 " rc=%d name=%s"
+		  "val=%p size=%lu", efs_fs, *ino, nid.f_hi, nid.f_lo, rc, name,
+		  value, size);
 	return rc;
 }
 
@@ -78,7 +89,7 @@ size_t efs_getxattr(struct efs_fs *efs_fs, efs_cred_t *cred,
 		     size_t *size)
 {
 	int rc;
-	dstore_oid_t oid;
+	node_id_t nid;
 	void *read_val = NULL;
 	size_t size_val;
 
@@ -88,10 +99,10 @@ size_t efs_getxattr(struct efs_fs *efs_fs, efs_cred_t *cred,
 	RC_WRAP_LABEL(rc, out, efs_access, efs_fs, cred, ino,
 		      EFS_ACCESS_READ);
 
-	RC_WRAP_LABEL(rc, out, efs_ino_to_oid, efs_fs, ino, &oid);
+	RC_WRAP_LABEL(rc, out, efs_ino_to_nodeid, ino, &nid);
 
-	RC_WRAP_LABEL(rc, out, md_xattr_get, &(efs_fs->kvtree->index), (obj_id_t *)&oid, name,
-		      &read_val, &size_val);
+	RC_WRAP_LABEL(rc, out, md_xattr_get, &(efs_fs->kvtree->index),
+		      &nid, name,  &read_val, &size_val);
 
 	/* The passed buffer size is insufficient to hold the xattr value */
 	if (*size < size_val) {
@@ -103,39 +114,33 @@ size_t efs_getxattr(struct efs_fs *efs_fs, efs_cred_t *cred,
 	*size = size_val;
 
 out:
-	log_trace("ctx=%p *ino=%llu oid=%" PRIx64 ":%" PRIx64 " rc=%d name=%s"
-		   " val=%p size=%lu", efs_fs, *ino, oid.f_hi, oid.f_lo, rc,
+	log_trace("ctx=%p *ino=%llu nid=%" PRIx64 ":%" PRIx64 " rc=%d name=%s"
+		   " val=%p size=%lu", efs_fs, *ino, nid.f_hi, nid.f_lo, rc,
 		   name, read_val, size_val);
 	if (read_val != NULL) {
 		md_xattr_free(read_val);
 	}
 	return rc;
-
 }
 
 int efs_removexattr(struct efs_fs *efs_fs, const efs_cred_t *cred,
 		    const efs_ino_t *ino, const char *name)
 {
 	int rc;
-	dstore_oid_t oid;
+	node_id_t nid;
 
 	RC_WRAP_LABEL(rc, out, efs_access, efs_fs, cred, ino,
 		      EFS_ACCESS_WRITE);
 
-	RC_WRAP_LABEL(rc, out, efs_ino_to_oid, efs_fs, ino, &oid);
+	RC_WRAP_LABEL(rc, out, efs_ino_to_nodeid, ino, &nid);
 
-	RC_WRAP_LABEL(rc, out, md_xattr_delete, &(efs_fs->kvtree->index), (obj_id_t *)&oid,
-		      name);
+	RC_WRAP_LABEL(rc, out, md_xattr_delete, &(efs_fs->kvtree->index),
+		      &nid, name);
 
 out:
-	log_trace("ctx=%p *ino=%llu oid=%" PRIx64 ":%" PRIx64 " rc=%d name=%s",
-		   efs_fs, *ino, oid.f_hi, oid.f_lo, rc, name);
+	log_trace("ctx=%p *ino=%llu nid=%" PRIx64 ":%" PRIx64 " rc=%d name=%s",
+		   efs_fs, *ino, nid.f_hi, nid.f_lo, rc, name);
 	return rc;
-}
-
-int efs_remove_all_xattr(struct efs_fs *efs_fs, efs_cred_t *cred, efs_ino_t *ino)
-{
-	return 0;
 }
 
 int efs_listxattr(struct efs_fs *efs_fs, const efs_cred_t *cred,
@@ -143,7 +148,7 @@ int efs_listxattr(struct efs_fs *efs_fs, const efs_cred_t *cred,
 		  size_t *size)
 {
 	int rc;
-	dstore_oid_t oid;
+	node_id_t nid;
 
 	dassert(cred != NULL);
 	dassert(ino != NULL);
@@ -154,14 +159,14 @@ int efs_listxattr(struct efs_fs *efs_fs, const efs_cred_t *cred,
 	RC_WRAP_LABEL(rc, out, efs_access, efs_fs, cred, ino,
 		      EFS_ACCESS_READ);
 
-	RC_WRAP_LABEL(rc, out, efs_ino_to_oid, efs_fs, ino, &oid);
+	RC_WRAP_LABEL(rc, out, efs_ino_to_nodeid, ino, &nid);
 
-	RC_WRAP_LABEL(rc, out, md_xattr_list, &(efs_fs->kvtree->index), (obj_id_t *)&oid, buf,
-		      count, size);
+	RC_WRAP_LABEL(rc, out, md_xattr_list, &(efs_fs->kvtree->index),
+		      &nid, buf, count, size);
 
 out:
-	log_trace("ctx=%p *ino=%llu oid=%" PRIx64 ":%" PRIx64 " rc=%d",
-		   efs_fs, *ino, oid.f_hi, oid.f_lo, rc);
+	log_trace("ctx=%p *ino=%llu nid=%" PRIx64 ":%" PRIx64 " rc=%d",
+		   efs_fs, *ino, nid.f_hi, nid.f_lo, rc);
 	return rc;
 }
 
