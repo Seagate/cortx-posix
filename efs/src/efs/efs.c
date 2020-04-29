bcd9a0307b5ddeb85ce84220919b360558408e34
Ujjwal Lanjewar <ujjwal.lanjewar@seagate.com>
2020-04-29 15:47:47 +0000

    Revert "Merge branch 'EOS-6359-config' into 'master'"
    This reverts merge request !44

--- efs/src/efs/efs.c
+++ efs/src/efs/efs.c
@@ -31,86 +31,54 @@
 #include <debug.h>
 #include <management.h>
 #include <nsal.h> /* nsal_init,fini */
-#include <ecfg.h> /* cfg_init,fini */
+
+static struct collection_item *cfg_items;
 
 static int efs_initialized;
 
-struct efs_module_state {
-	struct ecfg *config;
-	/* Add efs specific tunable attributes here */
-};
-
-static struct efs_module_state *state = NULL;
-
-/* create the efs state */
-int efs_module_state_init(struct efs_module_state **out)
-{
-	int rc = 0;
-	struct efs_module_state *st_local = NULL;
-
-	st_local = malloc(sizeof(struct efs_module_state));
-	if (st_local == NULL) {
-		rc = -ENOMEM;
-		goto out;
-	}
-	st_local->config = NULL;
-
-out:
-	*out = st_local;
-	return rc;
-}
-
-/* finalize the efs state */
-void efs_module_state_fini(struct efs_module_state *efs_state)
-{
-	free(efs_state);
-}
-
 int efs_init(const char *config_path)
 {
+	struct collection_item *errors = NULL;
 	int rc = 0;
-	char *log_path = NULL;
-	char *log_level = NULL;
-	struct ecfg *ecfg_local;
-	struct collection_item *cfg_items = NULL;
+	struct collection_item *item = NULL;
+        char *log_path = NULL;
+        char *log_level = NULL;
 
 	/** only initialize efs once */
 	if (__sync_fetch_and_add(&efs_initialized, 1)) {
 		return 0;
 	}
 
-	rc = efs_module_state_init(&state);
-	if (rc != 0) {
+	rc = config_from_file("libkvsns", config_path, &cfg_items,
+			      INI_STOP_ON_ERROR, &errors);
+	if (rc) {
+		free_ini_config_errors(errors);
+		rc = -rc;
 		goto out;
 	}
-	rc = cfg_init(config_path, &ecfg_local);
-	if (ecfg_local == NULL) {
-		goto out;
-	}
-	state->config = ecfg_local;
 
-	/* Fetch log-path & log-level */
-	rc = cfg_get(state->config, "log", "path", &log_path);
-	if (rc != 0) {
-		/* default log-path */
+	RC_WRAP(get_config_item, "log", "path", cfg_items, &item);
+
+	/** Path not specified, use default */
+	if (item == NULL) {
 		log_path = "/var/log/eos/efs/efs.log";
+	} else {
+		log_path = get_string_config_value(item, NULL);
 	}
-	rc = cfg_get(state->config, "log", "level", &log_level);
-	if (rc != 0){
-		/* default log-level */
+	item = NULL;
+
+	RC_WRAP(get_config_item, "log", "level", cfg_items, &item);
+	if (item == NULL) {
 		log_level = "LEVEL_INFO";
+	} else {
+		log_level = get_string_config_value(item, NULL);
 	}
+
 	rc = log_init(log_path, log_level_no(log_level));
         if (rc != 0) {
                 rc = -EINVAL;
                 goto out;
         }
-	/* Fetch collection-items */
-	rc = cfg_get_collection(state->config, &cfg_items);
-	if (rc != 0) {
-		log_err("ecfg_get_collection failed, rc=%d", rc);
-		goto log_cleanup;
-	}
 	rc = utils_init(cfg_items);
 	if (rc != 0) {
 		log_err("utils_init failed, rc=%d", rc);
@@ -149,8 +117,7 @@ log_cleanup:
 	log_fini();
 out:
 	if (rc) {
-		cfg_fini(state->config);
-		efs_module_state_fini(state);
+		free_ini_config_errors(errors);
 		return rc;
 	}
 
@@ -176,13 +143,12 @@ int efs_fini(void)
         }
 	rc = utils_fini();
 	if (rc) {
-		log_err("utils_fini failed, rc=%d", rc);
+        	log_err("utils_fini failed, rc=%d", rc);
         }
 	rc = log_fini();
 	if (rc) {
                 log_err("log_fini failed, rc=%d ", rc);
         }
-	cfg_fini(state->config);
-	efs_module_state_fini(state);
-	return rc;
+	free_ini_config_errors(cfg_items);
+        return rc;
 }
