86270b8974a9cba112a9fdc4b12759b68d30a50c
root <root@ssc-vm-c-325.colo.seagate.com>
2020-06-07 13:27:11 -0600

    Addressing review comments

--- experiments/readdir_profiling.c
+++ experiments/readdir_profiling.c
@@ -12,14 +12,16 @@
  * Author: Atita Shirwaikar <atita.shirwaikar@seagate.com>
 */
 
-/* This file has the implementation for the following experiment.
- * - Create 1000 files
- * - List 1000 files
- * - Delete 100 files
+/* This code has the implementation for the following experiment.
+ * - Create files
+ * - List files
+ * - Delete files
  * - Calculate time taken for above operation
+ * - NUM_FILES defines number of files to run the experiment for
 */
 #include "ut_efs_helper.h"
-#define DIR_NAME_LEN_MAX 255
+#define NUM_FILES 1000
+#define MAX_FILENAME_LENGTH 16
 #define DIR_ENV_FROM_STATE(__state) (*((struct ut_dir_env **)__state))
 
 struct ut_dir_env {
@@ -30,7 +32,7 @@ struct ut_dir_env {
 
 struct readdir_ctx {
 	int index;
-	char *readdir_array[1001];
+	char *readdir_array[NUM_FILES+1];
 };
 
 /**
@@ -79,12 +81,10 @@ static int dir_ops_setup(void **state)
 	int rc = 0;
 
 	struct ut_dir_env *ut_dir_obj = calloc(sizeof(struct ut_dir_env), 1);
-
 	ut_assert_not_null(ut_dir_obj);
 
-	ut_dir_obj->name_list = calloc(sizeof(char *), 1000);
-
-	if(ut_dir_obj->name_list == NULL) {
+	ut_dir_obj->name_list = calloc(sizeof(char *), NUM_FILES);
+	if (ut_dir_obj->name_list == NULL) {
 		rc = -ENOMEM;
 		ut_assert_true(0);
 	}
@@ -103,17 +103,19 @@ static int dir_ops_setup(void **state)
 static int dir_ops_teardown(void **state)
 {
 	int rc = 0;
+	struct ut_dir_env *ut_dir_obj = DIR_ENV_FROM_STATE(state);
 
+	free(ut_dir_obj->name_list);
 	rc = ut_efs_fs_teardown(state);
 	ut_assert_int_equal(rc, 0);
-	
+
 	free(*state);
 
 	return rc;
 }
 
 
-static int create_1000_files_teardown(void **state)
+static int create_files_teardown(void **state)
 {
 	int rc = 0, i=0;
 	time_t start_time, end_time;
@@ -121,10 +123,10 @@ static int create_1000_files_teardown(void **state)
 	struct ut_dir_env *ut_dir_obj = DIR_ENV_FROM_STATE(state);
 
 	time(&start_time);
-	printf("\nDeleting 1000 Files:Start time %s\n",ctime(&start_time));
+	printf("\nDeleting Files:Start time %s\n",ctime(&start_time));
 
-	/*Delete 1000 files*/
-	for (i=1;i<1001;i++)
+	/*Delete files*/
+	for (i=1;i<NUM_FILES+1;i++)
 	{
 		ut_dir_obj->ut_efs_obj.file_name = ut_dir_obj->name_list[i];
 		rc = ut_file_delete(state);
@@ -140,43 +142,47 @@ static int create_1000_files_teardown(void **state)
 	time(&end_time);
 	printf("Deleting Files:End time %s\n",ctime(&end_time));
 
-	for(i=0;i<1001;i++)
+	for (i=0;i<NUM_FILES+1;i++)
 	{
 		free(ut_dir_obj->name_list[i]);
 	}
-	
+
 	return rc;
 }
 
-/**Test to create 1000 files**/
-static int create_1000_files_setup(void **state)
+/**Test to create files**/
+static int create_files_setup(void **state)
 {
- 	int i=0,rc=0;
+	int i=0,rc=0;
 	time_t start_time, end_time;
 	struct ut_dir_env *ut_dir_obj = DIR_ENV_FROM_STATE(state);
 
-	ut_dir_obj->entry_cnt = 1000;
+	ut_dir_obj->entry_cnt = NUM_FILES;
 
-	/* Allocate space for 1000 entries */
-	for(i=0;i<1001;i++)
+	/* Allocate space for NUM_FILES files, plus 1 directory entry */
+	for (i=0;i<NUM_FILES+1;i++)
 	{
-		ut_dir_obj->name_list[i]=malloc(sizeof(char*));
+		ut_dir_obj->name_list[i]=malloc(MAX_FILENAME_LENGTH);
+		if (ut_dir_obj->name_list == NULL){
+			rc = -ENOMEM;
+			ut_assert_true(0);
+		}
 	}
-	
+
 	/* Create a directory under root*/
-	strcpy(ut_dir_obj->name_list[0], "Test_Dir");
+	strncpy(ut_dir_obj->name_list[0], "Test_Dir",strlen("Test_Dir")+1);
 	printf("Test dir %s\n",ut_dir_obj->name_list[0]);
 	ut_dir_obj->ut_efs_obj.file_name = ut_dir_obj->name_list[0];
 	rc = ut_dir_create(state);
 	ut_assert_int_equal(rc,0);
 
 	time(&start_time);
-	printf("\nStart time:Create 1000 files is %s\n", ctime(&start_time));
+	printf("\nStart time:Create %d files is %s\n", NUM_FILES,ctime(&start_time));
 
 	ut_dir_obj->ut_efs_obj.parent_inode = ut_dir_obj->ut_efs_obj.file_inode;
 
-	/* Create 1000 files */
-	for (i=1;i<1001;i++)
+	/* Create files under Test Directory*/
+	for (i=1;i<NUM_FILES+1;i++)
 	{
 		snprintf(ut_dir_obj->name_list[i],sizeof(char*),"%d",i+1);
 		ut_dir_obj->ut_efs_obj.file_name = ut_dir_obj->name_list[i];
@@ -184,16 +190,16 @@ static int create_1000_files_setup(void **state)
 		ut_assert_int_equal(rc,0);
 	}
 	time(&end_time);
-	printf("End time:Create 1000 files is %s\n", ctime(&end_time));
+	printf("End time:Create %d files is %s\n", NUM_FILES, ctime(&end_time));
 	return rc;
 }
 
-static void read_1000_files(void **state)
+static void read_files(void **state)
 {
 	int rc = 0;
 	time_t start_time, end_time;
 	efs_ino_t dir_inode = 0LL;
-	
+
 	struct ut_dir_env *ut_dir_obj = DIR_ENV_FROM_STATE(state);
 	struct ut_efs_params *ut_efs_obj = &ut_dir_obj->ut_efs_obj;
 
@@ -205,19 +211,19 @@ static void read_1000_files(void **state)
 	printf("\nStart time:Lookup %s\n",ctime(&start_time));
 
 	rc = efs_lookup(ut_efs_obj->efs_fs, &ut_efs_obj->cred,
-			         &ut_efs_obj->current_inode, ut_dir_obj->name_list[0],
-			         &dir_inode);
+			&ut_efs_obj->current_inode, ut_dir_obj->name_list[0],
+			&dir_inode);
 	ut_assert_int_equal(rc,0);
 	time(&end_time);
 	printf("End time:Lookup %s\n", ctime(&end_time));
 
 	time(&start_time);
-	printf("\nStart time:Read 1000 files %s\n", ctime(&start_time));
+	printf("\nStart time:Read %d files %s\n", NUM_FILES, ctime(&start_time));
 	rc = efs_readdir(ut_efs_obj->efs_fs, &ut_efs_obj->cred, &dir_inode,
-				       test_readdir_cb, readdir_ctx);
+			test_readdir_cb, readdir_ctx);
 	ut_assert_int_equal(rc, 0);
 	time(&end_time);
-	printf("End time:Read 1000 files %s\n", ctime(&end_time));
+	printf("End time:Read %d files %s\n", NUM_FILES, ctime(&end_time));
 	verify_dentries(readdir_ctx, ut_dir_obj, 1);
 
 	readdir_ctx_fini(readdir_ctx);
@@ -244,14 +250,14 @@ int main(void)
 	}
 
 	struct test_case test_list[] = {
-		ut_test_case(read_1000_files, create_1000_files_setup, create_1000_files_teardown),
+		ut_test_case(read_files, create_files_setup, create_files_teardown),
 	};
 
 	int test_count = sizeof(test_list)/sizeof(test_list[0]);
 	int test_failed = 0;
 
 	test_failed = ut_run(test_list, test_count, dir_ops_setup,
-				dir_ops_teardown);
+			dir_ops_teardown);
 
 	ut_fini();
 
