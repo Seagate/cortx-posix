f2158d13d43b57b673349d10ea91db1d84394dfe
Anilkumar Sindhi <anilkumar.sindhi@seagate.com>
2020-06-26 00:38:11 -0600

    EOS_8600: Integrated kvtree attach, detach, symlink set, get and del to efs

--- efs/src/efs/efs_internal.h
+++ efs/src/efs/efs_internal.h
@@ -21,6 +21,39 @@
 #include "efs.h"
 #include "kvnode.h"
 
+enum efs_sys_attr_type
+{
+   EFS_SYS_ATTR_SYMLINK = 1,
+   EFS_SYS_ATTR_MAX
+};
+
+/**
+ * A kvnode is identified by a 128 bit node id. However, currently efs uses
+ * 64 bit inodes in it's apis to identify the entities. The following apis
+ * are a temporary arrangement to convert 64 bit inodes to 128 bit node ids.
+ * @TODO: Cleanup these apis when efs filehandle (or equivalent) is
+ * implemented. The filehandles would eventually use 128bit unique fids.
+ */
+static inline int node_id_to_ino(const node_id_t *nid, efs_ino_t *ino)
+{
+	dassert(nid != NULL);
+	dassert(ino != NULL);
+
+	*ino = nid->f_hi;
+	return 0;
+}
+
+static inline int ino_to_node_id(const efs_ino_t *ino, node_id_t *nid)
+{
+	dassert(ino != NULL);
+	dassert(nid != NULL);
+
+	nid->f_hi = *ino;
+	nid->f_lo = 0;
+
+	return 0;
+}
+
 /** Set the extstore object identifier (kfid) with kvsns inode as the key */
 int efs_set_ino_oid(struct efs_fs *efs_fs, efs_ino_t *ino, dstore_oid_t *oid);
 
@@ -93,4 +126,41 @@ int efs_del_stat(struct kvnode *node);
  * @return - 0 on sucess on failure error code given by kvnode APIs
  */
 int efs_update_stat(struct kvnode *node, int flags);
+/*
+ * This API will store any type of system attributes associated with
+ * given node
+ *
+ * @param[in] node *    - node for which system attributes to be stored
+ * @param[in] value    - Buffer which needs to be be stored
+ * @param[in] attr_type - Type of system attirbutes to be stored
+ *
+ * @return - 0 on success else error code return by kvnode APIs
+ */
+int efs_set_sysattr(const struct kvnode *node, const buff_t value,
+		    enum efs_sys_attr_type attr_type);
+
+/*
+ * This API will fetch any type of system attributes associated with
+ * given node
+ *
+ * @param[in] node *    - node for which system attributes to be fetched
+ * @param[in] value *   - Ponter to buffer to store attributes
+ * @param[in] attr_type - Type of system attirbutes to be fetched
+ *
+ * @return - 0 on success else error code return by kvnode APIs
+ */
+int efs_get_sysattr(const struct kvnode *node, buff_t *value,
+		    enum efs_sys_attr_type attr_type);
+
+/*
+ * This API will delete any type of system attributes associated with
+ * given node
+ *
+ * @param[in] node *    - node for which system attributes to be deleted
+ * @param[in] attr_type - Type of system attirbutes to be deleted
+ *
+ * @return - 0 on success else error code return by kvnode APIs
+ */
+int efs_del_sysattr(const struct kvnode *node,
+		    enum efs_sys_attr_type attr_type);
 #endif
