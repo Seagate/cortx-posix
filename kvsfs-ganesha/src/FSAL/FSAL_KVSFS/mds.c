290384dc725159b018ee251d1bef087b8f7c1bb4
Swapnil Gaonkar <swapnil.gaonkar@seagate.com>
2020-08-11 12:09:35 +0530

    Revert "EOS-11661: EOS-NFS: Update copyright message in kvsfs-ganesha and script directory"
    This reverts commit 61bf8f7469cd5a805d4aab3837a55b9b85e7477a.

--- kvsfs-ganesha/src/FSAL/FSAL_KVSFS/mds.c
+++ kvsfs-ganesha/src/FSAL/FSAL_KVSFS/mds.c
@@ -1,20 +1,26 @@
 /*
- * Filename:         mds.c
- * Description:      
  *
- * Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Affero General Public License for more details.
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * For any questions about this software or licensing,
- * please email opensource@seagate.com or cortx-questions@seagate.com.* 
+ * vim:noexpandtab:shiftwidth=8:tabstop=8:
+ *
+ * Copyright © 2012 CohortFS, LLC.
+ * Author: Adam C. Emerson <aemerson@linuxbox.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
  */
 
 /* TODO:PORTING: pNFS support is disabled */
